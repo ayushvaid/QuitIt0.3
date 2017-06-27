1cfc9261df094f9ad746523bd055dddd894eacb2
Ayush Vaid <vaid.ayush@gmail.com>
2017-06-27 17:04:39 +0530

    sqliteJAISI!!

--- app/src/main/java/com/example/android/quitit/MainActivity.java
+++ app/src/main/java/com/example/android/quitit/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.android.quitit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,39 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button newEntry = (Button) findViewById(R.id.new_entry_button);
-        Button allEntries = (Button) findViewById(R.id.all_entries_button);
+        FloatingActionButton newEntryFab = (FloatingActionButton) findViewById(R.id.fab);
 
-        newEntry.setOnClickListener(new View.OnClickListener(){
+        newEntryFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, NewEntryActivity.class);
                 startActivity(i);
             }
         });
-
-        allEntries.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, AllEntries.class);
-                startActivity(i);
-            }
-        });
-
-
     }
-
-
-
-    public void newEntryMethod(View view)
-    {
-        Intent i = new Intent(MainActivity.this, NewEntryActivity.class);
-        startActivity(i);
-    }
-    public void viewAllEntriesMethod(View view)
-    {
-        Intent i = new Intent(MainActivity.this, EntriesListAdapter.class);
-        startActivity(i);
-    }
-
 }
